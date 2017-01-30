ca7d11598051097b848c445ba6087356b46885fb
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-01-30 18:46:06 -0200

    Add eslint and fix lint error on custom script

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # checkout-instore-custom
 Custom inStore scripts to go on stores like tbb
 
-## tbb
+## Setup
+
+npm install
+
+## Edit TBB Custom
 
 1. Edit file tbb-checkout-instore-custom.js and commit on this project.
 2. Copy the the value of this file on: https://instoretbb.vtexcommercestable.com.br/admin/portal/#/sites/default/code/files/checkout-instore-custom.js
--- tbb-checkout-instore-custom.js
+++ tbb-checkout-instore-custom.js
@@ -1,59 +1,60 @@
-(function(){
-    var isLoaded = false;
-    var maxRetries = 3, countRetries = 0;
+(function() {
+  var isLoaded = false
+  var maxRetries = 3
+  var countRetries = 0
 
-    function onload() {
-        if (!isLoaded) {
-            addCustomizationLinksToMenu();
-            customizeIdentification();
-            isLoaded = true;
+  function onload() {
+    if (!isLoaded) {
+      addCustomizationLinksToMenu()
+      customizeIdentification()
+      isLoaded = true
+    }
+  }
+
+  function addCustomizationLinksToMenu() {
+    window.dispatchEvent(new CustomEvent('addCustomLink.instore', {
+      detail: [
+        {title: 'Abertura', href: '/caixa-instore#abertura', 'section': 'Caixa'},
+        {title: 'Sangria', href: '/caixa-instore#sangria', 'section': 'Caixa'},
+        {title: 'Reforco', href: '/caixa-instore#reforco', 'section': 'Caixa'},
+        {title: 'Fechamento', href: '/caixa-instore#fechamento', 'section': 'Caixa'},
+        {title: 'Cancelamento', href: '/caixa-instore#cancelamento', 'section': 'Caixa'},
+        {title: 'Cadastro', href: '/cadastro-instore', 'section': 'Fidelidade'},
+      ],
+    }))
+  }
+
+  function customizeIdentification() {
+    function useIdentificationOnlyWithCPF() {
+      var identificationInput = document.getElementById('identification')
+      if (identificationInput) {
+        identificationInput.setAttribute('placeholder', 'CPF')
+        identificationInput.setAttribute('type', 'tel')
+      } else {
+        countRetries++
+        if (countRetries <= maxRetries) {
+          setTimeout(function() {
+            useIdentificationOnlyWithCPF()
+          }, 100)
         }
+      }
     }
 
-    function addCustomizationLinksToMenu(){
-        window.dispatchEvent(new CustomEvent('addCustomLink.instore', {
-            detail: [
-                {title: 'Abertura', href: '/caixa-instore#abertura', 'section': 'Caixa'},
-                {title: 'Sangria', href: '/caixa-instore#sangria', 'section': 'Caixa'},
-                {title: 'Reforco', href: '/caixa-instore#reforco', 'section': 'Caixa'},
-                {title: 'Fechamento', href: '/caixa-instore#fechamento', 'section': 'Caixa'},
-                {title: 'Cancelamento', href: '/caixa-instore#cancelamento', 'section': 'Caixa'},
-                {title: 'Cadastro', href: '/cadastro-instore', 'section': 'Fidelidade'}
-            ]
-        }));
-    }
-
-    function customizeIdentification() {
-        function useIdentificationOnlyWithCPF() {
-            var identificationInput = document.getElementById('identification');
-            if (identificationInput) {
-                identificationInput.setAttribute('placeholder', 'CPF');
-                identificationInput.setAttribute('type', 'tel');
-            } else {
-              countRetries++;
-              if (countRetries <= maxRetries) {
-                setTimeout(function(){
-                  useIdentificationOnlyWithCPF();
-                }, 100);
-              }
-            }
+    window.document.addEventListener('path.updated', function (e) {
+      countRetries = 0
+      setTimeout(function() {
+        var path = e.data.path
+        if (path === '/') {
+          useIdentificationOnlyWithCPF(path)
         }
+      }, 0)
+    }, false)
 
-        window.document.addEventListener('path.updated', function (e) {
-            countRetries = 0;
-            setTimeout(function(){
-                var path = e.data.path;
-                if (path === '/') {
-                    useIdentificationOnlyWithCPF(path);
-                }
-            }, 0)
-        }, false)
+    useIdentificationOnlyWithCPF()
+  }
 
-        useIdentificationOnlyWithCPF()
-    }
-
-    window.document.addEventListener('load.instore', onload);
-    if(window.inStoreIsLoaded) {
-        onload();
-    }
-})();
+  window.document.addEventListener('load.instore', onload)
+  if (window.inStoreIsLoaded) {
+    onload()
+  }
+})()
