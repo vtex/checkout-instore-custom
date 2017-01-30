945e21b32cb3c52b0011c3fd8901a663320667dc
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-01-30 18:24:16 -0200

    Add checkout-instore-custom

---
+++ checkout-instore-custom.js
@@ -0,0 +1,50 @@
+(function(){
+    var isLoaded = false;
+
+    function onload() {
+        if (!isLoaded) {
+            addCustomizationLinksToMenu();
+            customizeIdentification();
+            isLoaded = true;
+        }
+    }
+
+    function addCustomizationLinksToMenu(){
+        window.dispatchEvent(new CustomEvent('addCustomLink.instore', {
+            detail: [
+                {title: 'Abertura', href: '/caixa-instore#abertura', 'section': 'Caixa'},
+                {title: 'Sangria', href: '/caixa-instore#sangria', 'section': 'Caixa'},
+                {title: 'Reforco', href: '/caixa-instore#reforco', 'section': 'Caixa'},
+                {title: 'Fechamento', href: '/caixa-instore#fechamento', 'section': 'Caixa'},
+                {title: 'Cancelamento', href: '/caixa-instore#cancelamento', 'section': 'Caixa'},
+                {title: 'Cadastro', href: '/cadastro-instore', 'section': 'Fidelidade'}
+            ]
+        }));
+    }
+
+    function customizeIdentification() {
+        function useIdentificationOnlyWithCPF() {
+            var identificationInput = document.getElementById('identification');
+            if (identificationInput) {
+                identificationInput.setAttribute('placeholder', 'CPF');
+                identificationInput.setAttribute('type', 'tel');
+            }
+        }
+
+        window.document.addEventListener('path.updated', function (e) {
+            setTimeout(function(){
+                var path = e.data.path;
+                if (path === '/') {
+                    useIdentificationOnlyWithCPF(path);
+                }
+            }, 0)
+        }, false)
+
+        useIdentificationOnlyWithCPF()
+    }
+
+    window.document.addEventListener('load.instore', onload);
+    if(window.inStoreIsLoaded) {
+        onload();
+    }
+})();
