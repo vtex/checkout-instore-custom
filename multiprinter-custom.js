98d6aab9ad69f9b888843029905165cce71fa5d4
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-09-16 09:39:15 -0300

    Always change printer when vendor is changed script

--- multiprinter-custom.js
+++ multiprinter-custom.js
@@ -1,18 +1,36 @@
+window.ORDER_PLACED_HOOK = window.ORDER_PLACED_HOOK_GLOBAL = {
+  url: 'http://10.1.13.159:6060/api/vtex/pedido', // Notebook teste do inStore
+}
+
 const usernamePrinterEndpoint = {
   'guibruzzi@gmail.com': 'http://10.1.12.215:6060/api/vtex/pedido',
+  'arlindorodrigues@gmail.com': 'http://200.162.48.237:6060/api/vtex/pedido',
 }
 
-function setPrinter() {
-  setTimeout(function() {
-    const vendorState = window.flux.getStore('VendorStore').getState()
-    const vendor = vendorState.toJS().vendor || {}
-    const username = vendor.username
-    if (username && usernamePrinterEndpoint[username]) {
-      window.ORDER_PLACED_HOOK = {
-        url: usernamePrinterEndpoint[username],
-      }
+function setPrinter(vendor) {
+  const email = vendor && vendor.username
+  if (email && usernamePrinterEndpoint[email]) {
+    window.ORDER_PLACED_HOOK = {
+      url: usernamePrinterEndpoint[email],
     }
-  }, 3000)
+  } else {
+    window.ORDER_PLACED_HOOK = window.ORDER_PLACED_HOOK_GLOBAL
+  }
 }
 
-setPrinter()
+function getGlobalVendor() {
+  const vendorState = window.flux.getStore('VendorStore').getState()
+  return vendorState.toJS().vendor
+}
+
+document.addEventListener(
+  'vendorIdentified',
+  function(event) {
+    const data = event.data
+    const vendor = data.vendor
+    setPrinter(vendor)
+  },
+  false
+)
+
+setPrinter(getGlobalVendor())
