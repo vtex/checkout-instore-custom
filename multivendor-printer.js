ccaeec90ff01a383030d395dd8d75e2fce3fe274
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-09-17 21:00:54 -0300

    Format printer function to new config pattern

--- multivendor-printer.js
+++ multivendor-printer.js
@@ -2,17 +2,23 @@ window.ORDER_PLACED_HOOK = window.ORDER_PLACED_HOOK_GLOBAL = {
   url: 'http://10.1.13.159:6060/api/vtex/pedido', // Notebook teste do inStore
 }
 
-const usernamePrinterEndpoint = {
-  'guibruzzi@gmail.com': 'http://10.1.12.215:6060/api/vtex/pedido',
-  'arlindorodrigues@gmail.com': 'http://200.162.48.237:6060/api/vtex/pedido',
+const vendorConfig = {
+  'guibruzzi@gmail.com': {
+    orderPlacedHook: {
+      url: 'http://10.1.12.215:6060/api/vtex/pedido',
+    },
+  },
+  'arlindorodrigues@gmail.com': {
+    orderPlacedHook: {
+      url: 'http://200.162.48.237:6060/api/vtex/pedido',
+    },
+  },
 }
 
 function setPrinter(vendor) {
   const email = vendor && vendor.username
-  if (email && usernamePrinterEndpoint[email]) {
-    window.ORDER_PLACED_HOOK = {
-      url: usernamePrinterEndpoint[email],
-    }
+  if (email && vendorConfig[email] && vendorConfig[email].orderPlacedHook) {
+    window.ORDER_PLACED_HOOK = vendorConfig[email].orderPlacedHook
   } else {
     window.ORDER_PLACED_HOOK = window.ORDER_PLACED_HOOK_GLOBAL
   }
@@ -23,14 +29,18 @@ function getGlobalVendor() {
   return vendorState.toJS().vendor
 }
 
+function onVendorChange(vendor) {
+  setPrinter(vendor)
+}
+
 document.addEventListener(
   'vendorIdentified',
   function(event) {
     const data = event.data
     const vendor = data.vendor
-    setPrinter(vendor)
+    onVendorChange(vendor)
   },
   false
 )
 
-setPrinter(getGlobalVendor())
+onVendorChange(getGlobalVendor())
