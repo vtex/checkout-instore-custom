5e73f1a0d3fa991a09e31c605c91c907e33a8a10
Darlene <darlenedms3@gmail.com>
2017-10-02 19:14:51 -0300

    Add minimum value cart validation

--- custom-validations.js
+++ custom-validations.js
@@ -1,14 +1,119 @@
 window.INSTORE_CONFIG = {
+  salesChannel: 13,
   customerValidationRules: {
     params: {
-      _fields: 'flagCessado,MotivoBloqueio'
+      _fields: 'flagCessado,MotivoBloqueio,DataInicioCiclo,email'
     },
     expectedResult: {
       "flagCessado": false,
       "MotivoBloqueio": null,
     },
     customError: 'Representante está com pendência e deve entrar em contato com a supervisora',
+  },
+  cartMinimumValue: 10493
+}
+
+window.cartStatus = {
+  get: function() {
+    if (this.currentCartStatus !== undefined) {
+      return this.currentCartStatus
+    }
+
+    const orderForm = getOrderForm()
+    return handleCustomerValidations(orderForm)
+    .then(() => this.currentCartStatus)
+  },
+
+  set: function(valid) {
+    this.currentCartStatus = valid
   }
+}
+
+function formatAmount(amount) {
+  return (amount / 100).toLocaleString('pt-BR', { minimumFractionDigits: 2 })
+}
+
+function getCartMinimumValueDiff() {
+  orderFormValue = getOrderForm().value
+  diff = window.INSTORE_CONFIG.cartMinimumValue - orderFormValue
+  if (diff > 0) {
+    return diff
+  }
+  return 0
+}
+
+function generateInvalidCartMessage() {
+  const amount = getCartMinimumValueDiff()
+  return `Ainda faltam R$ ${formatAmount(amount)} para alcançar o valor mínimo da compra`
+}
+
+function dispatchCartValidation(isValid) {
+  cartValidationEvent = new Event('cartValidated.instore')
+  cartValidationEvent.data = {
+    error: !isValid,
+    errorMessage: isValid ? undefined : generateInvalidCartMessage()
+  }
+  document.dispatchEvent(cartValidationEvent)
+}
+
+function handleCartValidations() {
+  const valid = window.cartStatus.get()
+  dispatchCartValidation(valid)
+  return valid
+}
+
+function setCartStatus(valid) {
+  window.cartStatus.set(valid)
+  return valid
+}
+
+function hasValidCampaigns(campaigns) {
+  return campaigns.some(c => c.name === 'CARRINHOLIBERADO')
+}
+
+function handleResponse(data) {
+  if (data && data.status === 200) {
+    return data.json()
+  }
+  return []
+}
+
+function getMatchCampaigns(clientResult) {
+  if (!clientResult) {
+    return []
+  }
+
+  payload = {
+    id: null,
+    isShoppingCart: true,
+    origin: 'Marketplace',
+    profileId: null,
+    email: clientResult.email,
+    salesChannel: window.INSTORE_CONFIG.salesChannel,
+    utm_source: null,
+    utm_medium: null,
+    utm_campaign: null,
+    utmi_campaign: null,
+    forcePurchasedHistoryFrom: clientResult.DataInicioCiclo,
+    forcePurchasedHistoryTo: (new Date()).toISOString(),
+    params: [],
+  }
+
+  return fetch('/api/rnb/pub/match-campaigns', {
+    method: 'post',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify(payload)
+  })
+  .then(handleResponse)
+}
+
+function isValidCustomerCart(clientResult) {
+  return getMatchCampaigns(clientResult)
+  .then(hasValidCampaigns)
+  .then(setCartStatus)
+  .then(handleCartValidations)
 }
 
 function resultMatcher(expectedResult, result) {
@@ -39,13 +144,11 @@ function isValidCustomer(clientResult) {
   }
 
   const expectedResult = rules.expectedResult
-  resultMatcher(expectedResult, clientResult)
-  .then((isValid) => {
-    if (isValid) {
-      return clientResult
-    }
-    return dispatchCustomerError(rules)
-  })
+  const match = resultMatcher(expectedResult, clientResult)
+  if (match) {
+    return clientResult
+  }
+  return dispatchCustomerError(rules)
 }
 
 function handleCustomerValidations(data) {
@@ -53,12 +156,19 @@ function handleCustomerValidations(data) {
   const rules = window.INSTORE_CONFIG.customerValidationRules
   rules.params._where = `email=${clientData.email}`
 
-  window.vtexInstore.getClient(clientData.email, rules.params)
+  return window.vtexInstore.getClient(clientData.email, rules.params)
   .then(isValidCustomer)
+  .then(isValidCustomerCart)
 }
 
 document.addEventListener(
   'clientProfile.updated',
   (e) => handleCustomerValidations(e.data),
+  false
+)
+
+document.addEventListener(
+  'cart.change',
+  handleCartValidations,
   false
 )
