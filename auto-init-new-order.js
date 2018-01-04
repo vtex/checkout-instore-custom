6db48dc186a00db1a879f8a17b6c34e60a53f242
Guilherme Bruzzi <guibruzzi@gmail.com>
2018-01-04 19:48:25 -0200

    No console.log on auto-init

--- auto-init-new-order.js
+++ auto-init-new-order.js
@@ -1,7 +1,6 @@
 const TIME_TO_RESET = 300000 // 5 min
 document.addEventListener('path.updated', (event) => {
   const path = event && event.data && event.data.path
-  console.log('path', path)
   if (!path || path !== 'orderplaced') {
     return
   }
