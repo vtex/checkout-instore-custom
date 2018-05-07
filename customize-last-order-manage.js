ada914d5043fd102d69801d97e5593fc53b1273d
Guilherme Bruzzi <guibruzzi@gmail.com>
2018-05-07 01:21:38 -0300

    Add customize-last-order-manage example

---
+++ customize-last-order-manage.js
@@ -0,0 +1,26 @@
+window.INSTORE_CONFIG.customerOpenOrderLink = {
+  useCustomHandler: true,
+  handleClick: function (params) {
+    console.log('Custom handle click received', params)
+
+    var href = params.href
+
+    if (!href) {
+      console.log('Not valid href to find orderId: ' + href)
+      return
+    }
+
+    var orderId = href.split('orders/')[1]
+    orderId = orderId.split('&')[0]
+    if (!orderId) {
+      console.log('Not valid orderId: ' + orderId)
+      return
+    }
+
+    var myOrdersUrl = '/account/orders#/' + orderId
+
+    console.log('Redirecting to: ' + myOrdersUrl)
+
+    window.location.href = myOrdersUrl
+  },
+}
