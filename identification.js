10080f21c706fc005bd9a46330c3ce30193d0c0a
Lucas Bebber <lucas.bebber@vtex.com.br>
2018-05-07 16:14:07 -0300

    Create identification.js

---
+++ identification.js
@@ -0,0 +1,9 @@
+window.INSTORE_CONFIG = {
+  enableIdentificationTypes:{
+    // to enable QR Code identification
+    QRCode: true,
+    // to disable default identification methods
+    CPF: false,
+    Email: false,
+  },
+}
