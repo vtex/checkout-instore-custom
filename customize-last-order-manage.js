8fdda96eb54e689605ca52f0e099de5d6e59f4a9
Guilherme Bruzzi <guibruzzi@gmail.com>
2018-05-09 01:01:34 -0300

    Improve customize last order manage example with required cookie and fix prettier on instore-as-checkout

--- customize-last-order-manage.js
+++ customize-last-order-manage.js
@@ -1,4 +1,57 @@
-window.INSTORE_CONFIG.customerOpenOrderLink = {
+function setCookie(key, value, expires, path, domain, secure) {
+  path = path || '/'
+  if (!key || !value) return false
+  if (!key || /^(?:expires|max-age|path|domain|secure)$/.test(key)) {
+    return
+  }
+
+  var sExpires = ''
+  if (expires) {
+    switch (typeof expires) {
+      case 'number':
+        sExpires = `; max-age=${expires}`
+        break
+      case 'string':
+        sExpires = `; expires=${expires}`
+        break
+      case 'object':
+        if (expires.hasOwnProperty('toGMTString')) {
+          sExpires = `; expires=${expires.toGMTString()}`
+        }
+        break
+    }
+  }
+
+  document.cookie = `${key}=${value}${sExpires}${
+    domain ? `; domain=${domain}` : ''
+  }${path ? `; path=${path}` : ''}${secure ? '; secure' : ''}`
+}
+
+function getOneDayExpirationDate() {
+  var now = new Date()
+  var time = now.getTime()
+  time += 24 * 3600 * 1000
+  now.setTime(time)
+  return now
+}
+
+function getCookie(name) {
+  var value = '; ' + document.cookie
+  var parts = value.split('; ' + name + '=')
+  if (parts.length === 2) {
+    return parts
+      .pop()
+      .split(';')
+      .shift()
+  }
+}
+
+function getAccountName() {
+  var hostname = window.location.hostname
+  return hostname.split('.')[0]
+}
+
+var customerOpenOrderLink = {
   useCustomHandler: true,
   handleClick: function(params) {
     console.log('Custom handle click received', params)
@@ -17,10 +70,22 @@ window.INSTORE_CONFIG.customerOpenOrderLink = {
       return
     }
 
+    /* My Orders need this cookie */
+    setCookie(
+      'VtexIdclientAutCookie_' + getAccountName(),
+      getCookie('VtexIdclientAutCookie'),
+      getOneDayExpirationDate()
+    )
+
     var myOrdersUrl = '/account/orders#/' + orderId
 
     console.log('Redirecting to: ' + myOrdersUrl)
 
     window.location.href = myOrdersUrl
   },
+}
+
+window.INSTORE_CONFIG = {
+  // ...
+  customerOpenOrderLink: customerOpenOrderLink,
 }
--- instore-as-checkout.js
+++ instore-as-checkout.js
@@ -11,7 +11,7 @@
     function getCookie(name) {
       var value = '; ' + document.cookie;
       var parts = value.split('; ' + name + '=');
-      if (parts.length == 2) {
+      if (parts.length === 2) {
         return parts
           .pop()
           .split(';')
