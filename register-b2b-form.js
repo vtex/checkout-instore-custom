02a1351c004ae217369e09118267457d1c5fe94b
Guilherme Bruzzi <guibruzzi@gmail.com>
2018-06-26 19:32:14 -0300

    Add simple B2B example configurations

---
+++ register-b2b-form.js
@@ -0,0 +1,11 @@
+// Include corporate form with toggle link so it can be optional
+window.INSTORE_CONFIG = {
+  customerProfile: {
+    optional: true,
+  },
+}
+
+// Include corporate form as mandatory
+window.INSTORE_CONFIG = {
+  customerProfile: true,
+}
