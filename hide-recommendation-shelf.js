aa59372638cbd3cde6c5590a58a5f57a5258860d
Guilherme Bruzzi <guibruzzi@gmail.com>
2018-09-12 17:10:58 -0300

    Change recommendation settings example

---
+++ hide-recommendation-shelf.js
@@ -0,0 +1,10 @@
+/* Incluir no checkout-instore-custom.css do portal da loja */
+#recommendation-shelf {
+  display: none;
+}
+
+/* Incluir no checkout-instore-custom.js do portal da loja */
+
+window.INSTORE_CONFIG = {
+  recommendationsEnabled: false, // hidding recommendations (default is to show: true)
+}
