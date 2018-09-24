64ac337f0f7f9b482d460f3e430958c4f644fbbb
Guilherme Bruzzi <guibruzzi@gmail.com>
2018-09-24 18:22:24 -0300

    Add add-menu-item.js guide

---
+++ add-menu-item.js
@@ -0,0 +1,31 @@
+// Add the following code to checkout-instore-custom.js
+function addMenuItems() {
+  window.dispatchEvent(
+    new CustomEvent('addCustomLink.instore', {
+      detail: [
+        {
+          title: 'Charles Proxy',
+          href: 'http://www.charlesproxy.com/getssl',
+          section: 'dev',
+        },
+      ],
+    })
+  )
+}
+
+function instoreLoaded() {
+  // etc...
+  addMenuItems()
+}
+
+document.addEventListener(
+  'load.instore',
+  function() {
+    setTimeout(instoreLoaded)
+  },
+  false
+)
+
+if (window.inStoreIsLoaded) {
+  setTimeout(instoreLoaded)
+}
