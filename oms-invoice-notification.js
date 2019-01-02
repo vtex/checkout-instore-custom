ffb8917123dccc789aea4336b6ee11dc6f7e86ee
Guilherme Bruzzi <guibruzzi@gmail.com>
2019-01-02 18:16:41 -0200

    Invoice with OMS notification configuration example

---
+++ oms-invoice-notification.js
@@ -0,0 +1,6 @@
+window.INSTORE_CONFIG = {
+  orderPlacedHook: {
+      url: '/instore/functions/vtex.instore-functions-oms-invoice-notification-v0/run',
+      cancelUrl: '/instore/functions/vtex.instore-functions-oms-invoice-notification-v0/run',
+  },
+}
