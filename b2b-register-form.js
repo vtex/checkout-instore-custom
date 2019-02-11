dff6b89a5b579ce0bd6be717a8585b822753a85e
Carlos Matheus <carlosmatheusbs@gmail.com>
2019-02-11 15:30:16 -0200

    Update b2b-register-form

--- b2b-register-form.js
+++ b2b-register-form.js
@@ -1,11 +1,15 @@
 // Include corporate form with toggle link so it can be optional
 window.INSTORE_CONFIG = {
   customerProfile: {
-    optional: true,
+    isCorporate: {
+      optional: true,
+    },
   },
 }
 
 // Include corporate form as mandatory
 window.INSTORE_CONFIG = {
-  customerProfile: true,
+  customerProfile: {
+    isCorporate: true,
+  },
 }
