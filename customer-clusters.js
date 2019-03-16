efac5942633e9b4d327709e90a010f8c1539c58b
Guilherme Bruzzi <guibruzzi@gmail.com>
2019-03-16 13:55:47 -0300

    Create customer clusters example

---
+++ customer-clusters.js
@@ -0,0 +1,12 @@
+const customerClusters = [
+  { id: 'clusterMesAniversarioPercentual' },
+  { id: 'clusterMesAniversarioProgressivo' },
+  { id: 'clusterPJPercentual' },
+  { id: 'clusterSofa20102014' },
+  { id: 'clusterVip' },
+]
+
+window.INSTORE_CONFIG = {
+  // ...
+  customerClusters: customerClusters,
+}
