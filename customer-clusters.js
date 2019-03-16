ade8a61f766368ce9fd77141dee1901a05f4809e
Guilherme Bruzzi <guibruzzi@gmail.com>
2019-03-16 14:25:58 -0300

    Update customer clusters example

--- customer-clusters.js
+++ customer-clusters.js
@@ -1,9 +1,9 @@
-const customerClusters = [
-  { id: 'clusterMesAniversarioPercentual' },
-  { id: 'clusterMesAniversarioProgressivo' },
-  { id: 'clusterPJPercentual' },
-  { id: 'clusterSofa20102014' },
-  { id: 'clusterVip' },
+var customerClusters = [
+  { id: 'clusterMesAniversarioPercentual', label: 'Aniversário percentual' },
+  { id: 'clusterMesAniversarioProgressivo', label: 'Aniversário progressivo' },
+  { id: 'clusterPJPercentual', label: 'PJ Percentual' },
+  { id: 'clusterSofa20102014', label: 'Sofá 2010-2014' },
+  { id: 'clusterVip', label: 'Cliente VIP' },
 ]
 
 window.INSTORE_CONFIG = {
