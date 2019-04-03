3e390c93356bec44ec0f12e35761d69b33dd6c24
Guilherme Bruzzi <guibruzzi@gmail.com>
2019-04-03 15:45:09 -0300

    Add split payments example

---
+++ split-payments.js
@@ -0,0 +1,23 @@
+// To force split view:
+window.INSTORE_CONFIG = {
+  // ...
+  payments: {
+    nativeSplit: true,
+  },
+}
+
+// To name sellers to better inform the sales person on the screen:
+window.INSTORE_CONFIG = {
+  // ...
+  sellers: {
+    vtexinstoredev: {
+      name: 'E-Commerce',
+    },
+    lojadarlene: {
+      name: 'Loja física da Darlene',
+    },
+    lojabruzzi: {
+      name: 'Loja física do Bruzzi',
+    },
+  },
+}
