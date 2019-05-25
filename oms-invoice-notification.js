0ae6837fd3faba86dace1a98681d983188c0445e
Guilherme Bruzzi <guibruzzi@gmail.com>
2019-05-25 15:32:45 -0300

    Change OMS invoice API endpoint

--- oms-invoice-notification.js
+++ oms-invoice-notification.js
@@ -1,6 +1,8 @@
 window.INSTORE_CONFIG = {
   orderPlacedHook: {
-      url: '/instore/functions/vtex.instore-functions-oms-invoice-notification-v0/run',
-      cancelUrl: '/instore/functions/vtex.instore-functions-oms-invoice-notification-v0/run',
-  },
-}
+    url:
+      "/api/functions/vtex.instore-functions-oms-invoice-notification-v0/run?an=instore",
+    cancelUrl:
+      "/api/functions/vtex.instore-functions-oms-invoice-notification-v0/run?an=instore"
+  }
+};
