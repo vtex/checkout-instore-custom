69f5ec6667fcc14ad7c829820223c2dfa5ce7f07
Guilherme Bruzzi <guibruzzi@gmail.com>
2019-06-03 21:34:47 -0300

    add set-customer-identification example

---
+++ set-customer-identification.js
@@ -0,0 +1,21 @@
+function setCustomerIdentification(email) {
+  var customEvent = window.document.createEvent("Event");
+  customEvent.data = { email: email };
+  customEvent.initEvent("setCustomerIdentification", true, true);
+  window.document.dispatchEvent(customEvent);
+}
+
+function customizeCustomerIdentification() {
+  console.log("Ready to customize inStore customer identification");
+  // Do any request with global `fetch` or any global listening that needs and then call setCustomerIdentification(email)
+}
+
+if (window.customizeCustomerIdentification) {
+  customizeCustomerIdentification();
+}
+
+document.addEventListener(
+  "customizeCustomerIdentification",
+  customizeCustomerIdentification,
+  false
+);
