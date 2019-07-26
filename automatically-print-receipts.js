162c574bfb8661a6babc09c17f48aa64353ba433
Guilherme Bruzzi <guibruzzi@gmail.com>
2019-07-26 16:56:34 -0300

    Add automatically print receipts example

---
+++ automatically-print-receipts.js
@@ -0,0 +1,9 @@
+window.INSTORE_CONFIG = {
+  printingConfig: {
+    // This will make the receipts of payment cards to be automatically printed (you can configure to print both or just one)
+    printReceiptsAutomatically: {
+      merchant: true,
+      customer: true
+    }
+  }
+};
