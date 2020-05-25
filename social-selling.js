584533d54a6c00a471a9692397f1bfb198838712
Guilherme Bruzzi <guibruzzi@gmail.com>
2020-05-25 17:19:38 -0300

    Add social selling example

---
+++ social-selling.js
@@ -0,0 +1,11 @@
+// To configure social selling on inStore:
+window.INSTORE_CONFIG = {
+  // ...
+  socialShare: {
+    vendorType: "code",
+  },
+  noteAsVendorCode: {
+    type: "text",
+  },
+  ecommerceMenuItem: true,
+};
