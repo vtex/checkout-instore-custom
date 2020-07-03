e3018a8df23781e1e92092f30fae7d994950a92d
Guilherme Bruzzi <guibruzzi@gmail.com>
2020-07-03 10:28:36 -0300

    Add Register as mandatory step and with Invoice Address configuration example

---
+++ register-mandatory-and-with-invoice-address.js
@@ -0,0 +1,7 @@
+window.INSTORE_CONFIG = {
+  customerProfile: {
+    // other props...
+    forceRegisterOnSaleFlow: true, // Force always to register customer data
+    showInvoiceAddressOnRegister: true, // Add invoice address as mandatory information on this register form
+  },
+};
