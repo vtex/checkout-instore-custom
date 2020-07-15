f249472cf3f77f51f8d77d95244a4cffbe926142
Guilherme Bruzzi <guibruzzi@gmail.com>
2020-07-15 13:22:44 -0300

    Add login auth options example

---
+++ login-auth-options.js
@@ -0,0 +1,6 @@
+window.INSTORE_CONFIG = {
+  auth: {
+    showLoginAndPassword: true, // always show login and password (default: true). Set to false to hide login and password form
+    showSaml: true, // always show saml if it is configured on VTEX ID (default: true). Set to false to hide any saml button option
+  },
+};
