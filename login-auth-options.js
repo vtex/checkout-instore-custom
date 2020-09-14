974a582671b0bbdb5dcb20dc80c7d625950df4b8
Guilherme Bruzzi <guibruzzi@gmail.com>
2020-09-14 13:35:16 -0300

    Add Google provider on examples

--- login-auth-options.js
+++ login-auth-options.js
@@ -2,5 +2,6 @@ window.INSTORE_CONFIG = {
   auth: {
     showLoginAndPassword: true, // always show login and password (default: true). Set to false to hide login and password form
     showSaml: true, // always show saml if it is configured on VTEX ID (default: true). Set to false to hide any saml button option
+    oAuthProviderName: 'Google', // always show oauth this oauth if it is also configured on VTEX ID (default: undefined, to not show). Set to an valid oauth provider to VTEX ID knowledge and it will show it if there is no saml login also
   },
 }
