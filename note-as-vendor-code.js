6c8a3dd67e848fd76febd81aaa835575523d295f
Guilherme Bruzzi <guibruzzi@gmail.com>
2020-12-08 18:43:37 -0300

    Better not-as-vendor-code doc

--- note-as-vendor-code.js
+++ note-as-vendor-code.js
@@ -1,3 +1,4 @@
+// This config enables salespeople to mandatory insert a code identifying them on the sale, so they don't forget about it
 window.INSTORE_CONFIG = {
   noteAsVendorCode: {
     type: 'text',
