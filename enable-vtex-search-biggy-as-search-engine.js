1b818dd8761ff44d85b40545b42b87aef2551bbb
Guilherme Bruzzi <guibruzzi@gmail.com>
2020-12-11 11:08:20 -0300

    Add better VTEX search instructions

--- enable-vtex-search-biggy-as-search-engine.js
+++ enable-vtex-search-biggy-as-search-engine.js
@@ -1,3 +1,4 @@
+// You can enable by accounts (see custom-account-configuration.js) and vendors (see custom-vendor-configuration.js) also
 window.INSTORE_CONFIG = {
   search: {
     vtexSearch: true,
