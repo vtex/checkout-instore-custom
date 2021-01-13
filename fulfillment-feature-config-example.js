757123ef3cb8fd30f128b3e841cdc38b2f46e60f
Ivan de Freitas Jr <freitasjr.ivan@gmail.com>
2021-01-13 12:57:24 -0300

    add feature config example for fulfillment module

---
+++ fulfillment-feature-config-example.js
@@ -0,0 +1,16 @@
+// See also `fulfillment-ip-config-example.js` and `fulfillment-device-config-example.js`
+
+window.INSTORE_CONFIG = {
+  // ...
+  fulfillment: {
+    enabled: true, // Allow fulfillment menu option
+    features: { // Feature configs for instore fulfillment module
+      shipFromStore: { // Enable ship from store order fulfillment
+        enabled: true,
+      },
+      pickupInStore: { // Enable pickup in store order fulfillment
+        enabled: true,
+      },
+    },
+  },
+}
