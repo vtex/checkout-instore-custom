aaa3752ee30cd0bc628d598909f9ac0ae6b801e9
Everton Ataide <everton.ataide@vtex.com.br>
2021-05-25 13:50:39 -0300

    Add gift card configuration guide

---
+++ gift-card-config.js
@@ -0,0 +1,6 @@
+window.INSTORE_CONFIG = {
+  // This config will change the max length accepted on the Gift Card modal, can change it if the origin of the gift card is not the VTEX provider.
+  customGiftCardLength: 30,
+  // This config disable the mmask on the Gift Card modal (SSSS-SSSS-SSSS-SSSS), it's essential if you use a different origin that doesn't allow for the same format.
+  disableGiftCardMask: true,
+}
