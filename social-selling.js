87c5c38e3b8d159b0354a3644fbc2aa0c3f7d0b7
Arthur Sampaio <arthur.correia@vtex.com.br>
2021-06-03 09:14:21 -0300

    Update social-selling specs

--- social-selling.js
+++ social-selling.js
@@ -3,6 +3,7 @@ window.INSTORE_CONFIG = {
   // ...
   socialShare: {
     vendorType: 'code',
+    hostName: 'www.vt.ex/', // use this property in case to use shorterner URL
   },
   noteAsVendorCode: {
     type: 'text',
